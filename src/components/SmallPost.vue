8803681e4ad29fc6a54c0e8a37b22248d8ed50f2
Okano7-sys <abuzerokanerayabakan@outlook.com>
2024-07-28 03:55:14 +0300

    Update SmallPost.vue

--- src/components/SmallPost.vue
+++ src/components/SmallPost.vue
@@ -32,7 +32,7 @@ export default {
   border-radius: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   margin-bottom: 5px;
-  width: 200px; /* Daha küçük bir genişlik belirledik */
+  width: 200px; 
 }
 
 .post-header {
@@ -53,13 +53,13 @@ export default {
 .author {
   font-style: italic;
   color: black;
-  font-size: 14px; /* Yazı boyutunu biraz artırdık */
+  font-size: 14px; 
 }
 
 .post-image {
   width: 100%;
-  height: 100px; /* Sabit bir yükseklik belirledik */
-  object-fit: cover; /* Resmin konteynere sığmasını sağladık */
+  height: 100px; 
+  object-fit: cover; 
   border-radius: 5px;
 }
 
@@ -68,12 +68,12 @@ export default {
 }
 
 .post-info h3 {
-  font-size: 14px; /* Daha küçük başlık boyutu */
+  font-size: 14px; 
   margin: 5px 0;
 }
 
 .post-info p {
-  font-size: 12px; /* Daha küçük metin boyutu */
+  font-size: 12px; 
   margin: 0;
 }
 </style>
